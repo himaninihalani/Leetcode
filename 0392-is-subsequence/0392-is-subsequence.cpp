1a83957c8e105e36a2c0ad4a0aa9d1a89c36f23b
himaninihalani <himaninihalani14@gmail.com>
2025-10-26 20:54:53 +0530

    Time: 0 ms (100%), Space: 8.5 MB (90.24%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        int j=0;
+        while(i<s.size() && j<t.size()){
+            if(s[i]==t[j]){
+                i+=1;
+                
+            }
+            j+=1;
+        }
+    if(i == s.size()){
+        return true;
+    } 
+    return false;
+    }
+};
