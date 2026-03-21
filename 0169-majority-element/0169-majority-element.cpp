66738fb048c00242920d2d7ccfb22dc310178d4e
Himani <himaninihalani14@gmail.com>
2026-03-21 22:43:54 +0530

    Time: 0 ms (100%), Space: 28.3 MB (27.71%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,15 @@
+int hashh[1001]={0};
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        map<int , int> mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]>n/2){
+              return nums[i];
+            }
+        }
+       return 0; 
+    }
+};
