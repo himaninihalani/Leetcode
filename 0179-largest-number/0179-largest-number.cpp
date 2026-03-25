de99335f191890ea94f77039eb2512093a3ef77a
Himani <himaninihalani14@gmail.com>
2026-03-25 15:05:31 +0530

    Time: 8 ms (9.92%), Space: 16.4 MB (83.66%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,16 @@
+bool str(int a, int b){
+    return to_string(a)+to_string(b)>to_string(b)+to_string(a);
+}
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+       string ans;
+       sort(nums.begin(),nums.end(),str);
+       for(auto i:nums){
+         ans += to_string(i);
+       } 
+       if(ans[0]=='0') return "0";
+       return ans;
+       
+    }
+};
